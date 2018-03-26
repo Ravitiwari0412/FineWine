6c9e879b4db843e0f3678aed39a59d6f2dc6144b
Ravitiwari0412 <tiwaririshu19@gmail.com>
2018-03-26 13:20:18 +0530

    Initial commit

---
+++ app/src/main/java/com/codingblocks/finewine/Representativeactivity.java
@@ -0,0 +1,13 @@
+package com.codingblocks.finewine;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Representativeactivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_representativeactivity);
+    }
+}
